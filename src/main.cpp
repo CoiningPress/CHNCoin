646541f0788e0ca8e98ab3740d4e08fe067437dc
RoadTrain <pashtet92@bk.ru>
2013-11-01 06:04:41 +0400

    Modify pchMessageStart in order to drop nodes from other altcoins.

--- src/main.cpp
+++ src/main.cpp
@@ -2400,7 +2400,7 @@ bool static AlreadyHave(CTxDB& txdb, const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ascii, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // CHNCoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xfd, 0xc2, 0xb8, 0xdd }; // CHNCoin: increase each by adding 2 to litecoin's value.
 
 
 bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
